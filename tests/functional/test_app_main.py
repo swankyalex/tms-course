b32e7a61eacf46bbdf11ca111fdef3c0fc026776
swankyalex <72443505+swankyalex@users.noreply.github.com>
2020-10-20 20:34:56 +0300

    обновка теста для CI (#6)
    * Обновочка2
    
    * Обновочка2
    
    * Обновочка теста для CI

--- tests/functional/test_app_main.py
+++ tests/functional/test_app_main.py
@@ -21,9 +21,9 @@ def validate_title(page: MainPage):
 
 def validate_content(page: MainPage):
     assert page.h1.tag_name == "h1"
-    assert page.h1.text == "Z37 study project"
+    assert page.h1.text == "Z37 custom"
     assert page.p.tag_name == "p"
-    assert page.p.text == "This is a study project."
+    assert page.p.text == "This is a project."
 
     html = page.html
     assert "<hr>" in html
