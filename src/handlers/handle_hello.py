78ac49808096c81de61df15507ad222337b4f372
swankyalex <72443505+swankyalex@users.noreply.github.com>
2020-11-08 12:21:01 +0300

    Hello form (#11)

--- src/handlers/handle_hello.py
+++ src/handlers/handle_hello.py
@@ -1,13 +1,23 @@
-from framework.types import RequestT
 from framework.types import ResponseT
+from framework.utils import read_static
 
 
 def hello(request) -> ResponseT:
+    name = (request.query.get("name") or [None])[0]
+    address = (request.query.get("address") or [None])[0]
 
-    name = request.query.get("name") or "anon"
-    msg = f"Hello {name}"
+    base_html = read_static("_base.html").decode()
+    hello_html = read_static("hello.html").decode()
 
-    payload = msg.encode()
+    result = hello_html.format(
+        address_header=address or "nowhere",
+        address_value=address or "",
+        name_header=name or "бродяга",
+        name_value=name or "",
+    )
+    result = base_html.format(xxx=result)
+
+    payload = result.encode()
     status = "200 OK"
     headers = {
         "Content-type": "text/html",
